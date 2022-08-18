3f20cf4dbb631938774c65d8e4029a9d5fbe044b
E-Sahra-B <bendenizsahra@gmail.com>
2022-08-19 00:02:15 +0300

    TrafikLambası
    TrafikLambası

---
+++ TrafikLambasi/Form1.Designer.cs
@@ -0,0 +1,114 @@
+﻿
+namespace TrafikLambasi
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.button4);
+            this.panel1.Controls.Add(this.button3);
+            this.panel1.Controls.Add(this.button2);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Location = new System.Drawing.Point(12, 16);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(244, 308);
+            this.panel1.TabIndex = 0;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(122, 251);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(103, 37);
+            this.button4.TabIndex = 3;
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(13, 172);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(103, 73);
+            this.button3.TabIndex = 2;
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(13, 93);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(103, 73);
+            this.button2.TabIndex = 1;
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(13, 14);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(103, 73);
+            this.button1.TabIndex = 0;
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(269, 340);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ TrafikLambasi/Form1.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TrafikLambasi
+{
+    public partial class Form1 : Form
+    {
+        int sn = 80;
+        int t = 1;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            timer1.Start();
+        }
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            timer1.Interval = 500;
+            button4.Text = sn.ToString();
+            if (sn>0 && sn<40)
+            {
+                t = 3;
+            }
+            if (sn > 40 && sn < 45)
+            {
+                t = 2;
+            }
+            if (sn > 45 && sn < 80)
+            {
+                t = 1;
+            }
+            if (sn <=0)
+            {
+                sn = 80;
+            }
+            if (t==1)
+            {
+                sn--;
+                button1.BackColor = Color.Red;
+                button2.BackColor = Color.White;
+                button3.BackColor = Color.White;
+            }
+            if (t == 2)
+            {
+                sn--;
+                button1.BackColor = Color.White;
+                button2.BackColor = Color.Yellow;
+                button3.BackColor = Color.White;
+            }
+            if (t == 3)
+            {
+                sn--;
+                button1.BackColor = Color.White;
+                button2.BackColor = Color.White;
+                button3.BackColor = Color.Green;
+            }
+        }
+    }
+}
